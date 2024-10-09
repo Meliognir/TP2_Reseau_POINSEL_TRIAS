386ba79ad79fdf0413caecc1eeb0ce5680d33d88
trias <trias.nathan0@gmail.com>
2024-10-09 09:33:02 +0200

    Q1) corrected and Q2)

--- main.c
+++ main.c
@@ -8,16 +8,32 @@ typedef struct HEADER_TAG {
     long magic_number; /* 0x0123456789ABCDEFL */
 } HEADER;
 
+HEADER *freeBlocksList= NULL;
+
 void * malloc_3is(size_t size) {
-    HEADER* new_bloc = (HEADER *) sbrk(size + sizeof(HEADER));
+HEADER* new_bloc = (HEADER *) sbrk(size+sizeof(HEADER)+sizeof(long));
     if (new_bloc== (void *)-1) {
         return NULL;
     }
     new_bloc->bloc_size=size;
     new_bloc->ptr_next=NULL;
     new_bloc->magic_number=MAGIC_NUMBER;
+
 }
 
+void free_3is(HEADER *ptr) {
+    if (ptr!=NULL) {
+        return;
+    }
+    HEADER *block=ptr;
+    if(block->magic_number!=MAGIC_NUMBER) {
+        fprintf(stderr,"Erreur : corruption mémoire détectée \n)");
+        return;
+    }
+    block->ptr_next=freeBlocksList;
+    freeBlocksList=block;
+
+}
 
 
 int main(void) {
