cbccbdfa44fb9b1dcc32223b5649a2684a3beb56
trias <trias.nathan0@gmail.com>
2024-10-09 10:34:03 +0200

    Q1) Q2) Q3)

--- main.c
+++ main.c
@@ -11,32 +11,43 @@ typedef struct HEADER_TAG {
 HEADER *freeBlocksList= NULL;
 
 void * malloc_3is(size_t size) {
-HEADER* new_bloc = (HEADER *) sbrk(size+sizeof(HEADER)+sizeof(long));
-    if (new_bloc== (void *)-1) {
-        return NULL;
-    }
+    HEADER* new_bloc = (HEADER *) sbrk(size+sizeof(HEADER)+sizeof(long));
     new_bloc->bloc_size=size;
     new_bloc->ptr_next=NULL;
     new_bloc->magic_number=MAGIC_NUMBER;
-
+    return &(new_bloc->bloc_size);
 }
 
-void free_3is(HEADER *ptr) {
-    if (ptr!=NULL) {
+int check_3is(void *block) {
+    HEADER * block2 = block-1;
+
+    if(block2->magic_number!=(block2->ptr_next)->magic_number){
+        printf("Erreur : corruption mémoire détectée \n)");
+        return -1;
+    }
+    return 0;
+}
+
+
+
+void free_3is(HEADER *block) {
+    if (block==NULL) {
         return;
     }
-    HEADER *block=ptr;
-    if(block->magic_number!=MAGIC_NUMBER) {
-        fprintf(stderr,"Erreur : corruption mémoire détectée \n)");
-        return;
-    }
+    int osef = check_3is(block);
     block->ptr_next=freeBlocksList;
     freeBlocksList=block;
-
 }
+
 
 
 int main(void) {
+    void* stringTest = malloc_3is(3200);
+    sprintf(stringTest, "Hello world !\0");
+
+
+    printf("%s",stringTest);
+    free_3is(stringTest);
 
     return 0;
 }
